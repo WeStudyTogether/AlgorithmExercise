a6991a2c23981e98d7ab3c5bdd9d604c0f123826
Xingchao Long <z15881614642@163.com>
2020-01-23 16:20:44 +0800

    2020年1月23日16:20:40

--- AlgorithmExercise/src/main/java/com/fhlxc/week03/LongestCommonPrefix.java
+++ AlgorithmExercise/src/main/java/com/fhlxc/week03/LongestCommonPrefix.java
@@ -9,6 +9,20 @@ import java.io.InputStreamReader;
 * @date 2020/51/23 15:51:50
 * @ClassName LongestCommonPrefix
 * @Description 
+* 编写一个函数来查找字符串数组中的最长公共前缀。
+
+* 如果不存在公共前缀，返回空字符串 ""。
+
+* 示例 1:
+
+* 输入: ["flower","flow","flight"]
+* 输出: "fl"
+
+* 示例 2:
+
+* 输入: ["dog","racecar","car"]
+* 输出: ""
+* 解释: 输入不存在公共前缀。
 */
 
 public class LongestCommonPrefix {
@@ -16,6 +30,9 @@ public class LongestCommonPrefix {
     public static String longestCommonPrefix(String[] strs) {
         StringBuilder prefix = new StringBuilder();
         int min = Integer.MAX_VALUE;
+        if (strs.length == 0) {
+            return "";
+        }
         for (String s: strs) {
             int n = s.length();
             if (min > n) {
