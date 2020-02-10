f7fbf1f81880e5d56c6dd5bf6f4b0276c976b304
Xingchao Long <z15881614642@163.com>
2020-02-10 17:45:12 +0800

    修改题目描述

--- AlgorithmExercise/src/main/java/com/fhlxc/week06/Divide.java
+++ AlgorithmExercise/src/main/java/com/fhlxc/week06/Divide.java
@@ -9,6 +9,8 @@ import java.io.InputStreamReader;
 * @date 2020年2月10日 下午3:10:35
 * @ClassName Divide
 * @Description
+* 给定两个整数，被除数 dividend 和除数 divisor。将两数相除，要求不使用乘法、除法和 mod 运算符。
+* 返回被除数 dividend 除以除数 divisor 得到的商。
 */
 
 public class Divide {
