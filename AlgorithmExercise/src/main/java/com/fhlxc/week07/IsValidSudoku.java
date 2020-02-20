ed7874df88d92bd7fdd2289d150684d5ee451ec2
Xingchao Long <z15881614642@163.com>
2020-02-20 17:15:15 +0800

    添加信息

--- AlgorithmExercise/src/main/java/com/fhlxc/week07/IsValidSudoku.java
+++ AlgorithmExercise/src/main/java/com/fhlxc/week07/IsValidSudoku.java
@@ -8,6 +8,11 @@ import java.util.List;
 * @date 2020年2月20日 下午3:05:20
 * @ClassName IsValidSudoku
 * @Description 
+* 判断一个 9x9 的数独是否有效。只需要根据以下规则，验证已经填入的数字是否有效即可。
+*    数字 1-9 在每一行只能出现一次。
+*    数字 1-9 在每一列只能出现一次。
+*    数字 1-9 在每一个以粗实线分隔的 3x3 宫内只能出现一次。
+* 数独部分空格内已填入了数字，空白格用 '.' 表示。。
 */
 
 public class IsValidSudoku {
