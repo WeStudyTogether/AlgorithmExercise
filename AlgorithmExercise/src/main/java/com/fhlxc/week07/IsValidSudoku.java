a105bd5b7ace2a72f8e84a3008b31a1d8dae5b6d
Xingchao Long <z15881614642@163.com>
2020-02-20 16:38:40 +0800

    2020年02月20日

---
+++ AlgorithmExercise/src/main/java/com/fhlxc/week07/IsValidSudoku.java
@@ -0,0 +1,68 @@
+package com.fhlxc.week07;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+* @author Xingchao Long
+* @date 2020年2月20日 下午3:05:20
+* @ClassName IsValidSudoku
+* @Description 
+*/
+
+public class IsValidSudoku {
+
+    public boolean isValidSudoku(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            int raw = i / 3;
+            int column = i % 3;
+            int begin = raw * 27 + column * 3;
+            List<Character> list = new ArrayList<Character>();
+            for (int j = 0; j < 9; j++) {
+                int r = j / 3;
+                int c = j % 3;
+                int x = begin + r * 9 + c;
+                r = x / 9;
+                c = x % 9;
+                if (board[r][c] == '.') {
+                    continue;
+                }
+                for (Character ca: list) {
+                    if (ca == board[r][c]) {
+                        return false;
+                    }
+                }
+                list.add(board[r][c]);
+                for (int k = 1; k < 9 - c; k++) {
+                    if (board[r][c] == board[r][c + k]) {
+                        return false;
+                    }
+                }
+                for (int k = 1; k < 9 - r; k++) {
+                    if (board[r][c] == board[r + k][c]) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+    
+    public static void main(String[] args) {
+        char[][] board = {
+                {'.','.','.','.','5','.','.','1','.'},
+                {'.','4','.','3','.','.','.','.','.'},
+                {'.','.','.','.','.','3','.','.','1'},
+                {'8','.','.','.','.','.','.','2','.'},
+                {'.','.','2','.','7','.','.','.','.'},
+                {'.','1','5','.','.','.','.','.','.'},
+                {'.','.','.','.','.','2','.','.','.'},
+                {'.','2','.','9','.','.','.','.','.'},
+                {'.','.','4','.','.','.','.','.','.'}
+                };
+
+        IsValidSudoku isValidSudoku = new IsValidSudoku();
+        System.out.println(isValidSudoku.isValidSudoku(board));
+    }
+
+}
